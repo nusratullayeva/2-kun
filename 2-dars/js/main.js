87fdfb9aaecc3b4c5597a1a1ae20d852dc65a35f
nusratllayeva <anvarsaidmurotov@gmail.com>
2023-02-24 21:22:28 -0800

    initial

---
+++ 2-dars/js/main.js
@@ -0,0 +1,10 @@
+var idName = 'Ezoza';
+console.log(idName);
+var surName = 'Nusratullayeva';
+console.log(surName);
+var age = 13;
+console.log(age);
+var study = 'Najottalim';
+console.log(study);
+var use = 'hp';
+console.log(use);
